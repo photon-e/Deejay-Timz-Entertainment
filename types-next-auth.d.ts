3c6f7a741aa381e463aed13e72ea98192c9901c3
Elijah Ayomide raji <profyomiberg@gmail.com>
2026-06-17 21:50:31 +1000

    Patch Next.js for Netlify deploy

---
+++ types-next-auth.d.ts
@@ -0,0 +1,19 @@
+import type { DefaultSession } from 'next-auth';
+
+declare module 'next-auth' {
+  interface Session {
+    user?: DefaultSession['user'] & {
+      role?: 'Admin';
+    };
+  }
+
+  interface User {
+    role?: 'Admin';
+  }
+}
+
+declare module 'next-auth/jwt' {
+  interface JWT {
+    role?: 'Admin';
+  }
+}
